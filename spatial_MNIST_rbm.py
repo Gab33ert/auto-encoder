bc7d69a0c376a4fe88c6df73253a795be2f1d994
gouraud <gabriel.gouraud@polytechnique.edu>
2019-04-23 16:43:25 +0200

    spatial multilayer rbm inplemented but not working(only first layer converges)

--- spatial_MNIST_rbm.py
+++ spatial_MNIST_rbm.py
@@ -9,6 +9,8 @@ import copy
 from sklearn import preprocessing
 np.set_printoptions(threshold=np.inf)
 import tensorflow as tf
+import time
+from numpy.core.umath_tests import inner1d
 
 
 #build graph
@@ -93,7 +95,7 @@ def abstract_layer(in_index, W, t):#unfold the total connection matrix into laye
 
 #BACKPROP
 def sigmoid(x):
-    return 2/(1+np.exp(-x))-1
+    return (2/(1+np.exp(-x)))-1
 
 def dsigmoid(x):
     f=sigmoid(x)
@@ -163,36 +165,47 @@ def train_backprop(x_in, w, t, in_index, out_index, iterr, alpha):
 
 #RBM
 def sample_rbm_forward(visible, c, w):
-    return np.where(np.random.rand(w.shape[0],visible.shape[1]) < sigmoid(np.tile(c,(1,visible.shape[1]))+w.dot(visible)), 1, 0)
+    return np.where(np.random.rand(w.shape[0],visible.shape[1]) < sigmoid(np.tile(c,(1,visible.shape[1]))+w.dot(visible)), 1, -1)
 
-def sample_rbm_backward(hidden, b, w):
+def sample_rbm_backward(hidden, c, w):
+    return np.where(np.random.rand(w.shape[1],hidden.shape[1]) < sigmoid(np.tile(c,(1,hidden.shape[1]))+np.transpose(w).dot(hidden)), 1, -1)
+
+def sample_grbm_backward(hidden, b, w):
     return np.random.normal(np.tile(b,(1,hidden.shape[1]))+np.transpose(w).dot(hidden),1)#np.where(np.random.rand(w.shape[1],hidden.shape[1]) < sigmoid(np.tile(b,(1,hidden.shape[1]))+np.transpose(w).dot(hidden)), 0, 1) #this is no more sampling!!!!!!!!!!!!!
 
 def backandforw(hidden, b, c, w, k):
     for i in range(k):
-        visible_k=sample_rbm_backward(hidden, b, w)
+        if mode==0:
+            visible_k=sample_grbm_backward(hidden, b, w)
+        if mode==1:
+            visible_k=sample_rbm_backward(hidden, b, w)
         hidden=sample_rbm_forward(visible_k, c, w)
-        return hidden, visible_k
+    return hidden, visible_k
 
-def actualise_weight(visible, b, c, w, n, k):
+def actualise_weight(visible, b, c, w, n, k, t):
     hidden=sample_rbm_forward(visible, c, w)
     hidden_c, visible_c=backandforw(hidden, b, c, w, k)
-    #if n%10==0:print(np.mean(hidden,1))
-    #print((hidden.dot(np.transpose(visible)))[1,:]-(hidden.dot(np.transpose(visible)))[2,:])
-    #f=np.zeros((hidden.shape[0], visible.shape[0]))
-    #g=np.zeros((hidden.shape[0], 1))
-    #f[(n)%10,:]=np.ones((1,visible.shape[0]))
-    #g[n%10]=1
-    w+=(1/(visible.shape[1]))*epsilon_w*(hidden.dot(np.transpose(visible))-hidden_c.dot(np.transpose(visible_c)))#*f
-    b+=(1/(visible.shape[1]))*epsilon_b*(np.sum(visible-visible_c, axis=1)).reshape(visible.shape[0],1)
+    w+=(1/(visible.shape[1]))*epsilon_w*(hidden.dot(np.transpose(visible))-hidden_c.dot(np.transpose(visible_c)))*Wt[t]
+    if mode==0:
+        b+=(1/(visible.shape[1]))*epsilon_b*(np.sum(visible-visible_c, axis=1)).reshape(visible.shape[0],1)
     c+=(1/(visible.shape[1]))*epsilon_c*(np.sum(hidden-hidden_c, axis=1)).reshape(w.shape[0],1)#*g
-    e=0
-    for i in range(visible.shape[1]):
-        e-=(hidden[:,i]).dot(w.dot(visible[:,i]))
-    e+=-np.sum(b*visible)-np.sum(c*hidden)
-    return e/(visible.shape[1]), np.sum(np.sum(np.abs(visible-visible_c)))/(visible.shape[0]*visible.shape[1])
-  
-def train_rbm(visible, b, c, w, iterr_rbm):
+    """
+    if n%100==0:
+        e=0#energy on test set
+        E=0#training set
+        hidden_test=sample_rbm_forward(x_test, c, w)
+        for i in range(x_test.shape[1]):
+            e-=(hidden_test[:,i]).dot(w.dot(x_test[:,i]))
+        e+=-np.sum(b*x_test)-np.sum(c*hidden_test)
+        
+        hidden_train=sample_rbm_forward(x_train, c, w)
+        for i in range(x_train.shape[1]):
+            E-=(hidden_train[:,i]).dot(w.dot(x_train[:,i]))
+        E+=-np.sum(b*x_train)-np.sum(c*hidden_train)
+        return e/(x_test.shape[1]) #E/(x_train.shape[1])#, np.sum(np.sum(np.abs(visible-visible_c)))/(visible.shape[0]*visible.shape[1])
+    """
+    
+def train_rbm(visible, b, c, w, iterr_rbm, t):
     e=[]
     E=[]
     d=0
@@ -206,30 +219,57 @@ def train_rbm(visible, b, c, w, iterr_rbm):
             f=0
         if d>dataset_size-34:
             d=0
-        a, b = actualise_weight(visible_batch, b, c, w, i+1, 1)
-        e.append(a)
-        E.append(b)
+        actualise_weight(visible_batch, b, c, w, i+1, 1, t)
+        if i%50==0:
+            e.append(energy(x_test, b, c, w))
+            E.append(energy(visible, b, c, w))
     """
     for i in range(iterr_rbm//3):
-        a, b = actualise_weight(visible, b, c, w, i+1, 3)
-        e.append(a)
-        E.append(b)
+        actualise_weight(visible, b, c, w, i+1, 3, t)
+        if i%50==0:
+            e.append(energy(x_test, b, c, w))
+            E.append(energy(x_train, b, c, w))    
     """
-    plt.plot(e)
+    plt.plot(e,label="test set")
+    plt.plot(E, label="training set")
+    plt.legend(loc='upper right')
     plt.yscale('symlog')
     plt.show()
-    plt.plot(E)
-    plt.yscale('symlog')
-    plt.show()
-    
+    print("energy", e[len(e)-1])
+
+
+def energy(visible, b, c, w):
+    e=0
+    hidden=sample_rbm_forward(visible, c, w)
+    e+=-np.sum(b*visible)-np.sum(c*hidden)-np.sum(inner1d(hidden, w.dot(visible)))
+    return e/(visible.shape[1]) 
+        
+ 
 def gibbs_sampling(visible, b, c, w, n):
-    a=visible
-    plt.imshow(visible.reshape(28,28))
+    plt.imshow(np.transpose(scaler.inverse_transform(np.transpose(visible))).reshape(28,28), vmin=-100, vmax= 400)
+    plt.colorbar()
     plt.show()
+    a=visible
     for i in range(n):
         hidden=sample_rbm_forward(a, c, w)
-        visible=sample_rbm_backward(hidden, b, w)
-    plt.imshow(visible.reshape(28,28))
+        visible=sample_grbm_backward(hidden, b, w)
+    plt.imshow(np.transpose(scaler.inverse_transform(np.transpose(visible))).reshape(28,28), vmin=-100, vmax= 400)
+    plt.colorbar()
+    plt.show()
+    
+def gibbs_deep_sampling(visible, b, w, n):
+    #plt.imshow(np.transpose(scaler.inverse_transform(np.transpose(visible))).reshape(28,28), vmin=-100, vmax= 400)
+    #plt.colorbar()
+    #plt.show()
+    a=visible
+    hidden=sample_rbm_forward(a, b[1], w[0])
+    for i in range(n):
+        hidden=sample_rbm_forward(hidden, b[i+2], w[i+1])
+    for i in range(n):
+        hidden=sample_rbm_backward(hidden, b[n-i], w[n-i])
+    visible=sample_grbm_backward(hidden, b[0], w[0])
+    plt.imshow(np.transpose(scaler.inverse_transform(np.transpose(visible))).reshape(28,28), vmin=-100, vmax= 400)
+    plt.colorbar()
     plt.show()
 
 def error(visible, b,c,w):
@@ -260,27 +300,35 @@ def visualize(in_index, t):
 #global variable
 size=784
 n_cell=1600
-dataset_size=800
-dataset_size_t=400
+dataset_size=1000
+dataset_size_t=300
 t=5
-sigma=50
-d=310
+sigma=100
+d=480
 alpha=0.0005#backprop rate
 
-epsilon_w=0.01#rbm rate
+epsilon_w=0.001#rbm rate
 epsilon_b=epsilon_w
 epsilon_c=epsilon_w
-iterr_rbm=100
+
 
 
 P, W, in_index, out_index = build(n_cell, size, size,sparsity=0.05, seed=1)
 
 Wt=abstract_layer(in_index, W, t)
-wc=copy.deepcopy(Wt[0])
+w=[]
+b=[]
 
-wc=wc*(2*np.random.random(wc.shape)-1)*0.01
-c=0.01*np.random.randn(wc.shape[0],1)
-b=0.01*np.random.randn(wc.shape[1],1)
+for i in range(len(Wt)):
+    wc=copy.deepcopy(Wt[i])
+    #wc=np.ones(wc.shape)
+    wc=wc*np.random.normal(0, 0.01, wc.shape)#(2*np.random.random(wc.shape)-1)*0.01
+    w.append(wc)
+    b.append(np.zeros((wc.shape[1],1)))#0.01*np.random.randn(wc.shape[1],1))
+#b.append(0.01*np.random.randn(wc.shape[0],1))
+b[0]=0.01*np.random.randn(Wt[0].shape[1],1)
+b.append(np.zeros((wc.shape[0],1)))
+
 
 #wc=np.load("test.npy")
 #W=copy.deepcopy((wc != 0 ).astype(int))
@@ -292,8 +340,14 @@ x_train=x_train[0:dataset_size,:,:]
 x_test=x_test[0:dataset_size_t,:,:]
 
 
-x_train=np.transpose(np.asarray(x_train).reshape(dataset_size,-1))
-x_train=preprocessing.scale(x_train, axis=1)
+x_train=np.asarray(x_train).reshape(dataset_size,-1)
+scaler = preprocessing.StandardScaler().fit(x_train)
+x_train = np.transpose(scaler.transform(x_train))
+#x_train=preprocessing.scale(x_train, axis=1)
+
+
+x_test=np.asarray(x_test).reshape(dataset_size_t,-1)
+x_test=np.transpose(scaler.transform(x_test))#np.transpose(preprocessing.scale(x_test, axis=1))
 
     
 """
@@ -305,14 +359,82 @@ x_test=np.transpose(np.asarray(x_test).reshape(400,-1))
 x_t[in_index]=x_test
 """
 
-train_rbm(x_train, b, c, wc, iterr_rbm)
-for i in range(5):
-    plt.imshow(wc[i,:].reshape(28,28))
-    plt.show()
-    #plt.imshow(x_train[:,i].reshape(28,28))
-    #plt.show()
-print(error(x_train,b,c,wc))
-gibbs_sampling(x_train[:,0:1], b, c, wc, 10)
+x_test_copy=x_test
+
+
+visualize(in_index, t)
+answer = input("Do you want to keep going y/n?")
+if answer == "y":
+    mode=0#grbm mode
+    #layer 1
+    seconds = time.time()
+    iterr_rbm=2000
+    epsilon_w=0.01#rbm rate
+    epsilon_b=epsilon_w
+    epsilon_c=epsilon_w
+    train_rbm(x_train, b[0], b[1], w[0], iterr_rbm, 0)
+    for i in range(2):
+        plt.imshow(w[0][i,:].reshape(28,28))
+        plt.colorbar()
+        plt.show()
+    print("error", error(x_test,b[0],b[1],w[0]))
+    print("time",time.time()-seconds)
+    gibbs_sampling(x_test[:,0:1], b[0],b[1],w[0], 2)
+    gibbs_sampling(x_test[:,1:2], b[0],b[1],w[0], 2)
+    gibbs_sampling(x_test[:,20:21], b[0],b[1],w[0], 2)
+    
+    mode=1#rbm mode
+    
+    #layer 2
+    seconds = time.time()
+    iterr_rbm=500
+    epsilon_w=0.0000005#rbm rate
+    epsilon_b=epsilon_w
+    epsilon_c=epsilon_w
+    x_train_1=sample_rbm_forward(x_train, b[1], w[0])
+    x_test=sample_rbm_forward(x_test, b[1], w[0])
+    train_rbm(x_train_1, b[1],b[2],w[1], iterr_rbm, 1)
+    print("layer 2")
+    print("error", error(x_test,b[1],b[2],w[1]))
+    print("time",time.time()-seconds)
+    
+    #layer 3
+    seconds = time.time()
+    iterr_rbm=5000
+    epsilon_w=0.00005#rbm rate
+    epsilon_b=epsilon_w
+    epsilon_c=epsilon_w
+    x_train_2=sample_rbm_forward(x_train_1, b[2], w[1])
+    x_test=sample_rbm_forward(x_test, b[2], w[1])
+    train_rbm(x_train_2, b[2],b[3],w[2], iterr_rbm, 2)
+    print("layer 3")
+    print("error", error(x_test,b[2],b[3],w[2]))
+    print("time",time.time()-seconds)
+
+    #layer 4
+    seconds = time.time()
+    iterr_rbm=10000
+    epsilon_w=0.00001#rbm rate
+    epsilon_b=epsilon_w
+    epsilon_c=epsilon_w
+    x_train_3=sample_rbm_forward(x_train_2, b[3], w[2])
+    x_test=sample_rbm_forward(x_test, b[3], w[2])
+    train_rbm(x_train_3, b[3],b[4],w[3], iterr_rbm, 3)
+    print("layer 4")
+    print("error", error(x_test,b[3],b[4],w[3]))
+    print("time",time.time()-seconds)
+    
+    
+    gibbs_deep_sampling(x_test_copy[:,1:2], b, w, 0)
+    gibbs_deep_sampling(x_test_copy[:,1:2], b, w, 1)
+    gibbs_deep_sampling(x_test_copy[:,1:2], b, w, 2)
+    gibbs_deep_sampling(x_test_copy[:,1:2], b, w, 3)
+    
+elif answer == "n":
+    print("ok")
+else:
+    print("Please enter y or n.")
+
 
 """
 wc,e=train_backprop(x, wc, t, in_index, out_index, 100, alpha)
@@ -330,23 +452,3 @@ print(err(x, wc, t))
 print(err(x_t, wc, t)) 
 """
 
-"""
-plt.show()
-X, x=forward(x, wc, t)
-plt.plot(np.linspace(-1, 1, size),scaled_data)
-plt.plot(np.linspace(-1, 1, size),x[out_index])
-plt.show()
-
-X, x=forward(x_t[:,0:4], wc, t)
-
-
-for i in range(3):
-    plt.plot(np.linspace(-1, 1, size),scaled_data_t[:,i])
-    plt.plot(np.linspace(-1, 1, size),x[:,i][out_index])
-    plt.show()
-
-
-
-
-
-"""
